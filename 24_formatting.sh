5174ad9af892894e80c4878c9248fde6e6ef67e0
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2016-12-20 23:09:37 +0000

    Rubocop formatting

--- 24_formatting.sh
+++ 24_formatting.sh
@@ -2,14 +2,14 @@
 
 REPO=$1
 
-OUTPUT=$(rubocop --only Style --except StringLiterals,ConstantName,MethodName,PredicateName,VariableName -D $REPO/floodit.rb)
+OUTPUT=$(rubocop --only Style --except StringLiterals,ConstantName,MethodName,PredicateName,VariableName,Style/LeadingCommentSpace,Style/TrailingWhitespace,Style/EmptyLines -D $REPO/floodit.rb)
 
 if [ $? -eq 0 ]; then
     echo "[+] Source code is formatted according to style guide"
     exit 0
 else
     echo "[-] Source code is not formatted according to style guide:"
-    echo -e $OUTPUT
+    rubocop --only Style --except StringLiterals,ConstantName,MethodName,PredicateName,VariableName,Style/LeadingCommentSpace,Style/TrailingWhitespace,Style/EmptyLines -D $REPO/floodit.rb
     exit 1
 fi
 
