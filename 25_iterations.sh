c34d2030332677a0e726927b66486a198f610c4f
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2016-12-21 23:51:02 +0000

    Do not match in quoted strings

--- 25_iterations.sh
+++ 25_iterations.sh
@@ -2,16 +2,14 @@
 
 REPO=$1
 
-
-
 # Bad: for
-if cat $REPO/floodit.rb | sed -e 's/#.*$//g'  | grep -Eq '\bfor\b'; then
+if cat $REPO/floodit.rb | sed -e 's/"[^"].*"//g' | sed -e 's/#.*$//g'  | grep -Eq '\bfor\b'; then
     echo "[-] Use of standard for loops found"
     exit 1
 fi
 
 # Good: .each
-if cat $REPO/floodit.rb | sed -e 's/#.*$//g' | grep -Eq '\.each\b'; then
+if cat $REPO/floodit.rb | sed -e 's/"[^"].*"//g' | sed -e 's/#.*$//g' | grep -Eq '\.each\b'; then
     echo "[+] Found .each iteration"
     exit 0
 fi
