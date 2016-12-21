ccfc748d4d9e71d8609f7833cf0a942a1d89150a
Gordon Fraser <gordon.fraser@sheffield.ac.uk>
2016-12-21 23:22:14 +0000

    Fix globalvars in marking script

--- 22_modularised.sh
+++ 22_modularised.sh
@@ -10,12 +10,13 @@ OUTPUT=$(rubocop -c $RUBOCOP_FILE --only MethodLength,GlobalVars -D $REPO/floodi
 
 if [ $? -eq 0 ]; then
     echo "[+] Well modularised code"
+    rm $RUBOCOP_FILE
     exit 0
 else
     echo "[-] Not well modularised code. Here is the output of Rubocop:"
-    rubocop -c $RUBOCOP_FILE --only MethodLength -D $REPO/floodit.rb
+    rubocop -c $RUBOCOP_FILE --only MethodLength,GlobalVars -D $REPO/floodit.rb
+    rm $RUBOCOP_FILE
     exit 1
 fi
 
-rm $RUBOCOP_FILE
 
