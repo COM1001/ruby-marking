5801237b8505a4ae37c0b4324365d4fce3290242
Matthew Kinton <mfkinton1@sheffield.ac.uk>
2016-12-29 08:55:03 +0000

    Make main menu check work with splash

--- 09_main_menu.py
+++ 09_main_menu.py
@@ -33,13 +33,13 @@ of.close()
 
 command = "ruby %s" % workfile
 
-p = pexpect.spawn(command, env = {"GEM_HOME": "/home/codio/.gems", "GEM_PATH": "/home/codio/.gems", "PATH" : "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/codio/.gems/bin"})
+p = pexpect.spawn(command, env = {"GEM_HOME": "/home/codio/.gems", "GEM_PATH": "/home/codio/.gems", "PATH" : "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/codio/.gems/bin", "TERM": "linux"})
 fout = open('/home/codio/workspace/autograding_logs/09_main_menu.log','wb')
 p.logfile = fout
 p.setecho(True)
 p.sendline()
 try:
-  p.expect(re.compile('main menu', re.IGNORECASE), timeout=2)
+  p.expect([re.compile('main menu', re.IGNORECASE), re.compile('m.*a.*i.*n.*m.*e.*n.*u', re.IGNORECASE)], timeout=2)
   # Main menu
   p.sendline("s")
   p.expect([re.compile('.*turns.*', re.IGNORECASE), pexpect.TIMEOUT], timeout=2)
@@ -49,6 +49,8 @@ except:
   str = get_colorama_str()
   output = output.replace('\r\n', '').replace('\n', '').replace('0;39;', '')
   if not str in output:
+    print(str)
+    print(output)
     print("[-] Main menu option to start game does not work correctly")
     sys.exit(1)
 
@@ -56,7 +58,7 @@ try:
   p = pexpect.spawn(command, env = {"GEM_HOME": "/home/codio/.gems", "GEM_PATH": "/home/codio/.gems", "PATH" : "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/codio/.gems/bin"})
   p.logfile = fout
   p.sendline()
-  p.expect(re.compile('main menu', re.IGNORECASE), timeout=2)
+  p.expect([re.compile('main menu', re.IGNORECASE), re.compile('m.*a.*i.*n.*m.*e.*n.*u', re.IGNORECASE)], timeout=2)
 
   # Change size
   p.sendline("c")
@@ -64,7 +66,7 @@ try:
   p.sendline("5")
   p.expect(pexpect.TIMEOUT, timeout=2)
   p.sendline("5")
-  p.expect(re.compile('main menu', re.IGNORECASE), timeout=2)
+  p.expect([re.compile('main menu', re.IGNORECASE), re.compile('m.*a.*i.*n.*m.*e.*n.*u', re.IGNORECASE)], timeout=2)
 except:
   print("[-] Main menu option to change size does not work correctly")
   sys.exit(1)
